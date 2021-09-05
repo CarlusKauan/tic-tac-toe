85eb20fdabb11c8f2df1439e43389e3739c51d84
carloskauan <carloskauan190@gmail.com>
2021-09-05 18:38:33 -0300

    Finalizado v1.0 [JavaScript]

--- js/script.js
+++ js/script.js
@@ -11,6 +11,23 @@ const tic_tac_toe = {
     },
     container_element: null,
     gameover: false,
+    winning_sequences: [
+ 
+        //Horizontal
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+
+        //Vertical
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+
+        //Diagonal
+        [0,4,8],
+        [2,4,6]
+
+    ],
 
     init: function(container){
         this.container_element = container;
@@ -24,10 +41,43 @@ const tic_tac_toe = {
             
             // draw
             this.draw();
-            
-            // 'X' and 'O'
-            this.simbols.change();
+
+            let winning_sequences_index = this.check_winning_sequences ( this.simbols.options [ this.simbols.turn_index ]);
+            if(winning_sequences_index >= 0){
+                this.game_is_over();
+            } else {
+                // 'X' and 'O'
+                this.simbols.change();
+            }
+            return true;
+        } else{
+            return false;
         }
+    },
+
+    game_is_over: function(){
+        // alert("Game-Over");
+        this.gameover = true;
+        alert("Game-Over");
+        console.log("Game Over");
+    },
+
+    start: function(){
+        this.board.fill('');
+        this.draw();
+        this.gameover = false;
+    },
+
+    check_winning_sequences: function(simbol){
+        for (i in this.winning_sequences){
+            if(this.board [this.winning_sequences[i][0] ] == simbol &&
+               this.board [this.winning_sequences[i][1] ] == simbol &&
+               this.board [this.winning_sequences[i][2] ] == simbol){  
+                   console.log("Sequencia Vencedora !" + i);
+                   return i;
+            }
+        };
+        return -1;
     },
 
 
