8c7013f7ecee8a4c2fae43b104c6daee2a898589
carloskauan <carloskauan190@gmail.com>
2021-09-05 20:46:41 -0300

    Finalizado [Js]

--- js/script.js
+++ js/script.js
@@ -1,71 +1,90 @@
 //Creating a game on the object!
 const tic_tac_toe = {
     board: ['','','','','','','','',''],
-    simbols: {
+    symbols: {
         options: ['X','O'],
         turn_index: 0,
         change: function(){
             this.turn_index = (this.turn_index === 0 ? 1 : 0);
-        }
-    
+        } 
     },
     container_element: null,
     gameover: false,
     winning_sequences: [
- 
         //Horizontal
         [0,1,2],
         [3,4,5],
         [6,7,8],
-
         //Vertical
         [0,3,6],
         [1,4,7],
         [2,5,8],
-
         //Diagonal
         [0,4,8],
         [2,4,6]
-
     ],
 
     init: function(container){
         this.container_element = container;
     },
 
-    make_play: function(position){
-        if(this.gameover) return false;
-        if(this.board[position] === ''){
-            //turn
-            this.board[position] = this.simbols.options [ this.simbols.turn_index ];
-            
-            // draw
-            this.draw();
+    //makePlay
+    make_play(position) {
+        if (this.gameover || this.board[position] !== '') return false;
 
-            let winning_sequences_index = this.check_winning_sequences ( this.simbols.options [ this.simbols.turn_index ]);
-            if(winning_sequences_index >= 0){
-                this.game_is_over();
-            } else {
-                // 'X' and 'O'
-                this.simbols.change();
-            }
-            return true;
-        } else{
-            return false;
+        const currentSymbol = this.symbols.options[this.symbols.turn_index];
+        this.board[position] = currentSymbol;
+        this.draw();
+
+        const winning_sequences_index = this.check_winning_sequences(currentSymbol);
+        if (this.is_game_over()){
+            this.game_is_over();
         }
+        if (winning_sequences_index >= 0) {
+            this.game_is_over();
+            this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
+        } else {
+            this.symbols.change();
+        }
+
+        return true;
     },
+
+
+    //Stylize winner
+    stylize_winner_sequence(winner_sequence) {
+        winner_sequence.forEach((position) => {
+          this
+            .container_element
+            .querySelector(`div:nth-child(${position + 1})`)
+            .classList.add('winner');
+        });
+      },
 
     game_is_over: function(){
         // alert("Game-Over");
         this.gameover = true;
-        alert("Game-Over");
         console.log("Game Over");
+    },
+
+    is_game_over() {
+        return !this.board.includes('');
     },
 
     start: function(){
         this.board.fill('');
         this.draw();
         this.gameover = false;
+    },
+
+    restart() {
+        if (this.is_game_over() || this.gameover) {
+            this.start();
+            console.log('this game has been restarted!')
+        } else if (confirm('Are you sure you want to restart this game?')) {
+            this.start();
+            console.log('this game has been restarted!')
+        }
     },
 
     check_winning_sequences: function(simbol){
@@ -80,16 +99,12 @@ const tic_tac_toe = {
         return -1;
     },
 
-
     draw: function(){
         let content = '';
 
         for ( i in this.board) {
             content += '<div onclick="tic_tac_toe.make_play(' + i + ')">'+ this.board[i] +'</div>';
         }
-
         this.container_element.innerHTML = content;
     }
-
-
 };
